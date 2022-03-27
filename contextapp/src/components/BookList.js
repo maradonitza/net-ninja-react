c89334e0e1619226c6921882cbf0f11de56a3f68
maradonitza <maradonitza@gmail.com>
2022-03-27 17:46:20 +0300

    Accessing Context using contextType

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component
 {
+    static contextType = ThemeContext;
     render() {
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme? light : dark;
+
         return (
-            <div className='book-list'>
+            <div className='book-list' style = {{color: theme.syntax, background: theme.bg}}>
                 <ul>
                     <li>the way of kings</li>
                     <li>the name of the wind </li>
