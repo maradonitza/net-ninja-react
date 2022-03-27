d80acde803c646447eddc53e85ac0255dd2c8538
maradonitza <maradonitza@gmail.com>
2022-03-27 16:56:45 +0300

    Add context

---
+++ contextapp/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component, createContext} from 'react';
+
+class Navbar extends Component 
+{
+    render() {
+        return (
+            <nav>
+                <h1>Context App</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
+
+   
