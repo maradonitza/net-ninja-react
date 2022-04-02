66487fd63b11f8eada3e78a5b1651a3478d40766
maradonitza <maradonitza@gmail.com>
2022-04-03 02:29:37 +0300

    Access context using context type

--- demos/contextapp/src/components/BookList.js
+++ demos/contextapp/src/components/BookList.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react'; 
+import {AuthContext} from '../contexts/AuthContext';
 
 class BookList extends Component 
 { 
+    static contextType = AuthContext;
     render() { 
+        const {isAuthenticated, anotherProperty} = this.context;
         return (
             <div className='book-list'>
+                {isAuthenticated? 'Logged in': 'Logged out'}
                 <ul> 
                     <li>Three men in a boat</li> 
                     <li>Rumi's book of poetry</li> 
+                    <li>{anotherProperty}</li> 
                 </ul> 
             </div> 
         ); 
